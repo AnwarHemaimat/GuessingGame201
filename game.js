74bf219f25c4d78bd8b7e2851d8a0c1a40338d3f
AnwarHemaimat <Anwarhomaimat@gmail.com>
2021-03-01 21:55:20 +0200

    New game about me

--- game.js
+++ game.js
@@ -1,44 +1,34 @@
-confirm("This is a guessing game, be ready my frinde");
-alert("You should write Yes Or No and maybe Y Or N"+" "+"be ready")
-let userName=prompt("Please provide your name")
+confirm("This is a guessing game, be ready my friend");
+alert("You should write Yes Or No and maybe Y Or N,"+" "+"be ready");
+
+let userName=prompt("Please provide your name");
 console.log(userName)
-
 let myCountry=prompt("I am from Jordan ! ");
 console.log(myCountry)
 let myStudy=prompt("I studied Languages !");
 console.log(myStudy)
-let myAge=prompt("My age is over than 25 !");
+let myAge=prompt("Do you think my age is less than 25 !")
 console.log(myAge)
-let myHobby=prompt("Riding horse is my hobby !");
+let myHobby=prompt("Painting is my hobby !");
 console.log(myHobby)
 let myCourse=prompt("Now I am a student in LTUC - ASAC !");
 console.log(myCourse)
 
-while (myCountry==null || myStudy==null || myAge==null || myHobby==null || myCourse==null || myCountry=="" || myStudy=="" || myAge=="" || myHobby=="" || myCourse=="" ){
-    alert("Please sure if you answer all the questions");
-let myCountry=prompt("I am from Jordan ! ");
-console.log(myCountry)
-let myStudy=prompt("I studied Languages !");
-console.log(myStudy)
-let myAge=prompt("My age is over than 25 !");
-console.log(myAge)
-let myHobby=prompt("Riding horse is my hobby !");
-console.log(myHobby)
-let myCourse=prompt("Now I am a student in LTUC - ASAC !");
-console.log(myCourse)
-}
-
-if(myCountry.toUpperCase=='Yes' || myStudy.toUpperCase=='Yes' || myAge.toUpperCase=='Yes' || myHobby.toUpperCase=='Yes' || myCourse.toUpperCase=='Yes'){
-    alert("Welcome"+"  "+userName+" I'm so glad you tried to find out some information about me. Let me show you who I am ." );
-}
-if(myCountry.toUpperCase=='No' || myStudy.toUpperCase=='No' || myAge.toUpperCase=='No' || myHobby.toUpperCase=='No' || myCourse.toUpperCase=='No'){
-    alert("Welcome"+"  "+userName+" I'm so glad you tried to find out some information about me. Let me show you who I am ." );
-}
-
-if(myCountry.toUpperCase=='Y' || myStudy.toUpperCase=='Y' || myAge.toUpperCase=='Y' || myHobby.toUpperCase=='Y' || myCourse.toUpperCase=='Y'){
-    alert("Welcome"+"  "+userName+" I'm so glad you tried to find out some information about me. Let me show you who I am ." );
-}
-
-if(myCountry.toUpperCase=='N' || myStudy.toUpperCase=='N' || myAge.toUpperCase=='N' || myHobby.toUpperCase=='N' || myCourse.toUpperCase=='N'{
-    alert("Welcome"+"  "+userName+" I'm so glad you tried to find out some information about me. Let me show you who I am ." );
+while(myCountry==null || myCountry=="" || myStudy==null || myStudy=="" || myAge==null || myAge=="" || myHobby==null || myHobby=="" || myCourse==null || myCourse==""){
+    let userName=prompt("Please provide your name");
+    console.log(userName)
+    let myCountry=prompt("I am from Jordan ! ");
+    console.log(myCountry)
+    let myStudy=prompt("I studied Languages !");
+    console.log(myStudy)
+    let myAge=prompt("Do you think my age is less than 25 !")
+    console.log(myAge)
+    let myHobby=prompt("Painting is my hobby !");
+    console.log(myHobby)
+    let myCourse=prompt("Now I am a student in LTUC - ASAC !");
+    console.log(myCourse)
+if(myCountry=='Yes' || myStudy=='Yes' || myAge=='Yes' || myHobby=='Yes' || myCourse=='Yes' ||myCountry=='No' || myStudy=='No' || myAge=='No' || myHobby=='No' || myCourse=='No' || myCountry=='Y' || myStudy=='Y' || myAge=='Y' || myHobby=='Y' || myCourse=='Y' || myCountry=='N' || myStudy=='N' || myAge=='N' || myHobby=='N' || myCourse=='N'){
+    alert("Welcome my friend "+" "+userName+" , " + "I'm so glad you tried to find out some information about me. Let me show you who I am .");
+} else
+    alert("Please enter (Yes) Or (No) or (Y) Or (N) ,"+" "+"try again");
 }
