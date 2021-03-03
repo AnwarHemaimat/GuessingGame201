f51062d634669d7ac87a4f351545f9dcd8954e64
AnwarHemaimat <Anwarhomaimat@gmail.com>
2021-03-03 10:29:53 +0200

    fixed

--- game.js
+++ game.js
@@ -3,6 +3,11 @@ alert("You should write Yes Or No and maybe Y Or N,"+" "+"be ready");
 let sum=0;
 let userName=prompt("Please provide your name");
 console.log(userName)
+while(userName==null || userName==""){
+    alert("Please don't left your name empty !")
+    let userName=prompt("Please provide your name");
+    console.log(userName)
+}
 alert("Hello"+" "+userName+ ","+"I hope you are fine, and welcome you in my guessing game.");
 
 let myCountry=prompt("I am from Jordan ! ");
@@ -82,43 +87,44 @@ let guess=prompt("Enter my age if you can guess, is between 25 and 30")
 console.log(guess)
 while(guess==null || guess==""){
     alert("please answer with integer number about my age");
-    let guess=prompt("Enter my age if you can guess, is between 25 and 30")
+    let guess=prompt("Enter my age if you can guess again , is between 25 and 30")
     console.log(guess)
 }
 for(let count=0 ; count < 5 ; count++){
-    switch(guess){
-        case parseInt(guess)!==29:
+    while(guess!=29){
             alert("No, Please try again !");
-    
+            let guess=prompt("Enter my age if you can guess, is between 25 and 30")
+            console.log(guess)
+    }
             if(parseInt(guess)==29){
                 alert("Yes, this is true, my age is 29 years old.");
                 sum=sum+1;
                 count=4;
             }
     }
-}
 
 let cars = ["mercedes", "bmw", "ford", "jeep", "honda", "land cruiser"];
 let carsUser=prompt("Can you guess what my favorite car is ?")
 console.log(carsUser)
 while(carsUser==null || carsUser==""){
     alert("please answer with integer number about my age");
-    let carsUser=prompt("Can you guess what my favorite cars are ?")
+    let carsUser=prompt("Can you guess what my favorite cars are ?");
     console.log(carsUser)
 }
 
 for(let count2=0 ; count2<7 ; count2++){
-    switch(carsUser){
-        case carsUser.toLowerCase!==cars:
-        alert("No, try again please.");
-        
-        if(carsUser.toLowerCase==cars){
+    while(carsUser!=cars){
+        alert("No, Please try again !");
+        let carsUser=prompt("Can you guess what my favorite cars are again ?")
+        console.log(carsUser)
+}       
+        if(carsUser.toLowerCase()==cars){
             alert("Yes, this is true. Actually My favorite cars are: mercedes, bmw, ford, jeep, honda and land cruiser.");
             sum=sum+1;
             count2=6;
         }
 }
-}
+
 let counting=7;
 counting=counting-sum;
 alert("Welcome my friend "+" "+userName+" , " + "I'm so glad you tried to find out some information about me. Let me show you who I am .");
